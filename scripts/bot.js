150dc1cebcf6d6511cce655e1b43ae22efa350c4
Vanessa Brewster <vanessa@leadinghand.com.au>
2016-08-31 19:05:28 +1000

    change

--- scripts/bot.js
+++ scripts/bot.js
@@ -2,4 +2,8 @@ module.exports = function(robot){
   bot.respond(/What's your favorite food?/, function(res) {
    return res.send("I'm a robot--I don't eat food!");
  });
+
+ bot.hear(/Hello/, function(res){
+   return res.send("Hi there!")
+ })
 }
