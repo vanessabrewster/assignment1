32ca3ead78e956767813b48071913c0168ad17a5
Vanessa Brewster <vanessa@leadinghand.com.au>
2016-08-31 19:14:48 +1000

    adding wildcard selector

--- scripts/bot.js
+++ scripts/bot.js
@@ -6,4 +6,14 @@ module.exports = function(bot){
  bot.hear(/Hello/, function(res){
    return res.send("Hi there!")
  })
+
+ bot.respond(/Hi Hubot! My name is (.*)/i, function(msg){
+   let name;
+   name = msg.match[1];
+   if (name== "Hubot"){
+     return msg.send("You're not Hubot--I'm Hubot!");
+   } else {
+     return msg.reply("Nice to meet you," + name + "!");
+   }
+ });
 }
