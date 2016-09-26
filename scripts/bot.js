1540e2c9d0cc9b65b7a3bc09dc4f1582c30f8435
Vanessa Brewster <vanessa@leadinghand.com.au>
2016-09-26 18:12:36 +1000

    pushing to heroku

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,20 +1,57 @@
-module.exports = function(bot){
-  bot.respond(/What's your favorite food?/, function(res) {
-   return res.send("I'm a robot--I don't eat food!");
- });
+module.exports = function(robot) {
 
- bot.hear(/Hello/, function(res){
-   return res.send("Hi there!")
- })
+    robot.hear(/asl?/, function(res){
+        let asl = ["24, F, Sydney", "60, M, Brisbane", "12, F, Tasmania"];
+        let randomasl = asl[Math.floor(Math.random()*3)];
+        return res.send(randomasl);
+    });
 
- bot.respond(/Hi Hubot! My name is (.*)/i, function(msg){
-   let name;
-   name = msg.match[1];
-   if (name == "Hubot"){
-     return msg.send("You're not Hubot--I'm Hubot!");
-   } else {
-     return msg.reply("Nice to meet you," + name + "!");
-   }
- });
+    function getEmoji(emotion){
+      let sadEmoji = ["😞", "😒", "😫"];
+      let happyEmoji = ["😀", "😊", "😌"];
+
+      if (emotion === "sad") {
+        return sadEmoji[Math.floor(Math.random() * 3)];
+      }
+
+      if (emotion === "happy") {
+        return happyEmoji[Math.floor(Math.random() * 3)];
+      }
+    }
+
+    robot.respond(/I'm (.*)/i, function (res){
+      let randomSadEmotions = ["sad", "bummed", "unhappy", "devo"];
+      let randomHappyEmotions = ["happy", "elated", "great", "the best"]
+
+      if (randomSadEmotions.includes(res.match[1])){
+        return res.send(getEmoji("sad"));
+      }
+
+      if (randomHappyEmotions.includes(res.match[1])){
+        return res.send(getEmoji("happy"));
+      }
+
+    })
+
+    robot.hear(/What did you have for (.*)/i, function(res){
+      let meal = res.match[1];
+      switch(meal) {
+        case 'dinner?':
+          return res.send('Burgers');
+          break;
+        case 'lunch?':
+          return res.send('Salad');
+          break;
+        case 'brunch?':
+          return res.send('French Toast');
+          break;
+        case 'breakfast?':
+          return res.send('Eggs');
+          break;
+        default:
+          return res.send('Huh?');
+      }
+    })
+
 
 }
